2b4c5d94cd7246aecbe07ccf544bab1b71228d7e
Vimalsri5158 <125827626+Vimalsri5158@users.noreply.github.com>
2023-10-16 11:00:18 +0530

    changes

--- routes/mongoose-connection.js
+++ routes/mongoose-connection.js
@@ -1,10 +1,6 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-
-
-
-
 dotenv.config();
 
 const username = process.env.DB_USERNAME || '';
@@ -12,7 +8,7 @@ const password = process.env.DB_PASSWORD || '';
 const clusterName = process.env.DB_CLUSTER || '';
 const dbName = process.env.DB_NAME || '';
 
-const cloudMongoUrl = `mongodb+srv://${username}:${password}@${clusterName}/${dbName}?retryWrites=true&w=majority`;
+const cloudMongoUrl = `mongodb+srv://backend:9Z2GXXOW4Batxcr8@${clusterName}/${dbName}?retryWrites=true&w=majority`;
 
 const localMongoUrl ='mongodb://127.0.0.1:27017/backend'
 
