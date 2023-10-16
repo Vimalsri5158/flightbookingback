bc5bc808f3979a7f941b321ba08b3b7c323fa0b5
Vimalsri5158 <125827626+Vimalsri5158@users.noreply.github.com>
2023-10-16 11:12:56 +0530

    commit

--- routes/mongoose-connection.js
+++ routes/mongoose-connection.js
@@ -3,21 +3,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const username = process.env.DB_USERNAME || '';
-const password = process.env.DB_PASSWORD || '';
-const clusterName = process.env.DB_CLUSTER || '';
-const dbName = process.env.DB_NAME || '';
+const username =process.env.DB_USERNAME || '';
+const password =process.env.DB_PASSWORD || '';
+const clusterName =process.env.DB_CLUSTER || '';
+const dbName =process.env.DB_NAME || '';
 
-const cloudMongoUrl = `mongodb+srv://backend:9Z2GXXOW4Batxcr8@${clusterName}/${dbName}?retryWrites=true&w=majority`;
+const cloudMongoUrl=`mongodb+srv://backend:9Z2GXXOW4Batxcr8@cluster0.qoymzsx.mongodb.net/backend?retryWrites=true&w=majority`;
 
 const localMongoUrl ='mongodb://127.0.0.1:27017/backend'
 
 const connectToDb = async () => {
     try {
-
-        const mongoUrl = process.env.NODE_ENV === 'production' ? cloudMongoUrl : localMongoUrl;
-
-        await mongoose.connect(mongoUrl, {
+        await mongoose.connect(cloudMongoUrl, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
@@ -26,6 +23,6 @@ const connectToDb = async () => {
         console.error('MongoDB connection error:', err.message);
         process.exit(1);
     }
-};
+}
 
 export default connectToDb;
