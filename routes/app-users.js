8323ddc66e84d076832a8cb99aa83f2df7170b77
Vimalsri5158 <125827626+Vimalsri5158@users.noreply.github.com>
2023-10-26 23:50:11 +0530

    commit

--- routes/app-users.js
+++ routes/app-users.js
@@ -26,7 +26,6 @@ authRouter.post("/register", async (req, res) => {
       name: payload.name,
       email: payload.email,
       password: hashedPassword,
-      role: "admin",
     });
 
     // Save the new user to the database
